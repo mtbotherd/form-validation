022847677adba518dab098d78cfd2720dc7cf9df
richbothe <richbothe@gmail.com>
2018-10-27 19:02:52 -0500

    Form validation - positioning error for checkbox.

--- src/js/scripts2.js
+++ src/js/scripts2.js
@@ -60,34 +60,50 @@ $(document).ready(function() {
             // Add the `help-block` class to the error element
             error.addClass("help-block");
 
-            // Add `has-feedback` class to the parent div.form-group
+            // Add `has-feedback` class to the parent div.input-container
             // in order to add icons to inputs
-            element.parents(".col-md-12").addClass("has-feedback");
+            element.parents(".input-container").addClass("has-feedback");
 
-            if (element.prop("type") === "checkbox") {
-                error.insertAfter(element.parent("label"));
-            } else {
+            //error.insertAfter(element);
+
+            // Error message for checkbox
+            if (element.attr("name") === "checkbox") {
+                error.insertAfter(element.parent("div"));
+            }
+            else {
                 error.insertAfter(element);
             }
 
+            // if (element.attr("name") === "radio") {
+            //     error.insertAfter(element.parent("label"));
+            // } else {
+            //     error.insertAfter(element);
+            // }
+            //
+            // if (element.attr("name") == "checkboxGroup"){
+            //     error.insertAfter("label[for='checkbox3']");
+            // } else {
+            //     error.insertAfter(element);
+            // }
+
             // Add the div element, if doesn't exists, and apply the icon classes to it.
             if (!element.next("div")[0]) {
-                $("<div class='fas fa-exclamation-triangle fa-lg form-control-feedback'></div>").insertAfter(element);
+                $("<div class='fas fa-exclamation-triangle fa-sm form-control-feedback'></div>").insertAfter(element);
                 // $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
             }
         },
         success: function(label, element) {
             // Add the div element, if doesn't exists, and apply the icon classes to it.
             if (!$(element).next("div")[0]) {
-                $("<div class='fas fa-check fa-lg form-control-feedback'></div>").insertAfter($(element));
+                $("<div class='fas fa-check form-control-feedback'></div>").insertAfter($(element));
             }
         },
         highlight: function(element, errorClass, validClass) {
-            $(element).parents(".col-md-12").addClass("has-error").removeClass("has-success");
+            $(element).parents(".input-container").addClass("has-error").removeClass("has-success");
             $(element).next("div").addClass("fa-exclamation-triangle").removeClass("fa-check");
         },
         unhighlight: function(element, errorClass, validClass) {
-            $(element).parents(".col-md-12").addClass("has-success").removeClass("has-error");
+            $(element).parents(".input-container").addClass("has-success").removeClass("has-error");
             $(element).next("div").addClass("fa-check").removeClass("fa-exclamation-triangle");
         }
     });
