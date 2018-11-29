5a3214b10ef4873c2180072692ad89c24c0e3b58
mtbotherd <mtbotherd@users.noreply.github.com>
2018-11-29 12:51:01 -0600

    Modified file upload to show file name after selection.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    
+
+    // File upload - show file value after file selection.
+    $('.custom-file-input').on('change', function() {
+        var fileName = document.getElementById("fileUploadField").files[0].name;
+        $(this).next('.custom-file-label').addClass("selected").html(fileName);
+    })
 });
